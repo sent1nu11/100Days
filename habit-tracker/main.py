ac23452ea57fc7a9f26298489850f5f3bf524ade
sent1nu11 <sent1nu11@gmail.com>
2022-02-04 08:57:46 -0700

    point to Pixela Graph

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,27 +1,27 @@
 import requests
 
-USERNAME = "rogerw"
-TOKEN = "sduf8esuerwew"
+USERNAME = "sent1nu11"
+TOKEN = "sdfieru38flvcsdf"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token":TOKEN,
-    "username":USERNAME,
-    "agreeTermsOfService":"yes",
-    "notMinor":"yes"
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
 }
 
-#response = requests.post(url=pixela_endpoint, json=user_params)
-#print(response.text)
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/USERNAME/graphs"
 
 graph_config = {
-    "id": "graph1",
-    "name": "Cycling Graph",
-    "unit": "Km",
-    "type": "float",
-    "color": "sora",
+   "id": "graph1",
+   "name": "Cycling Graph",
+   "unit": "Km",
+   "type": "float",
+   "color": "sora",
 }
 
 response = requests.post(url=graph_endpoint, json=graph_config)
