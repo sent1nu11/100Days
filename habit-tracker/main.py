5093420077c98f32df61a5786f9a7e74e7530d6e
sent1nu11 <sent1nu11@gmail.com>
2022-02-05 07:11:05 -0700

    create graph

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -11,10 +11,10 @@ user_params = {
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
-graph_endpoint = f"{pixela_endpoint}/USERNAME/graphs"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
    "id": "graph1",
@@ -24,5 +24,9 @@ graph_config = {
    "color": "sora",
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config)
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
