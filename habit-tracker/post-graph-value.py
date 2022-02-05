581bc46626d509ef28eee7012d647d01b27619b9
sent1nu11 <sent1nu11@gmail.com>
2022-02-05 11:17:24 -0700

    finish habit tracking app

--- habit-tracker/post-graph-value.py
+++ habit-tracker/post-graph-value.py
@@ -1,9 +1,7 @@
 import requests
-from datetime import datetime
 
 USERNAME = "sent1nu11"
 TOKEN = "sdfieru38flvcsdf"
-GRAPH_ID = "graph1"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
@@ -13,34 +11,27 @@ user_params = {
     "notMinor": "yes"
 }
 
+data_params = {
+    "date": "20220204",
+    "quantity": "33.5"
+}
+
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+data_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1"
 
 graph_config = {
-   "id": GRAPH_ID,
+   "id": "graph1",
    "name": "Cycling Graph",
    "unit": "Km",
    "type": "float",
    "color": "sora",
 }
 
-pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
-
-today = datetime.now()
-print(today.strftime("%Y%m%d"))
-
-pixel_data = {
-    "date": "20220129",
-    "quantity": "30"
-}
-
 headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-
-
-response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+response = requests.post(url=data_endpoint, json=data_params, headers=headers)
 print(response.text)
