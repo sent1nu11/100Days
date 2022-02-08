9908968bd432c52355bd78e7773dbe1e466229ce
sent1nu11 <sent1nu11@gmail.com>
2022-02-08 05:13:47 -0700

    finished exercise tracker

--- sheety_app/main.py
+++ sheety_app/main.py
@@ -2,22 +2,37 @@ import requests
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
+X_APP_ID = os.getenv("APP_ID")
+X_APP_KEY = os.getenv("APP_KEY")
+
+GENDER = "female"
+WEIGHT_KG = 72.5
+HEIGHT_CM = 167.64
+AGE = 30
+
+print(X_APP_ID)
+
 nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 headers = {
-    "x-app-id": "5155317e",
-    "x-app-key": "5271e3295f34724841bbbbbc4dcd347c",
-    "x-remote-user-id": 0
+    "x-app-id": X_APP_ID,
+    "x-app-key": X_APP_KEY
 }
 
 user_params = {
     "query": "ran 3 miles",
-    "gender": "female",
-    "weight_kg": 72.5,
-    "height_cm": 167.64,
-    "age": 30
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
 }
+#
+# response = requests.get(url=nutritionix_endpoint)
+# print(response)
 
-response = requests.post(url=nutritionix_endpoint, json=user_params, headers= headers)
-print (response.text)
+response = requests.post(url=nutritionix_endpoint, json=user_params, headers=headers)
+result = response.json()
+print(result)
 
