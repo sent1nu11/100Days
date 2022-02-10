6c09c33d7c83c489be9dee9b2fdfbad0d9a0b984
sent1nu11 <sent1nu11@gmail.com>
2022-02-10 09:56:52 -0700

    added data_manager.py

--- personal_flight_club/main.py
+++ personal_flight_club/main.py
@@ -1,1 +1,10 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+import requests
+import data_manager
+from pprint import pprint
+
+response = requests.get(url="https://api.sheety.co/ad8d11e0d75ff57d00d6d5660e56066b/flightDeals2/prices")
+data = response.json()
+
+pprint(data)
