4f382d9fb11142fa5b5deece0a71eef8780005c0
sent1nu11 <77199919+sent1nu11@users.noreply.github.com>
2022-02-11 22:52:46 -0700

    add

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -11,8 +11,8 @@ user_params = {
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/USERNAME/graphs"
 
@@ -24,5 +24,12 @@ graph_config = {
    "color": "sora",
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config)
+# response = requests.post(url=graph_endpoint, json=graph_config)
+# print(response.text)
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
