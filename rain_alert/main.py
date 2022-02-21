dcfad976b9c9908dc8d146ccb8d173f0cd2d55c8
sent1nu11 <77199919+sent1nu11@users.noreply.github.com>
2022-02-21 15:57:03 -0700

    config.py

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -2,6 +2,7 @@ import requests
 from twilio.rest import Client
 import config
 
+
 account_sid = config.TWILIO_ACCOUNT_SID
 auth_token = config.TWILIO_AUTH_TOKEN
 client = Client(account_sid, auth_token)
