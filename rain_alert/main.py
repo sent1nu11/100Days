ee71984aeca3e495bfcc1ec8acc1908519464c87
sent1nu11 <77199919+sent1nu11@users.noreply.github.com>
2022-02-21 16:07:40 -0700

    removed phone numbers from main.py rain_alert

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -9,9 +9,9 @@ client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+15017122661',
-                     to='+15558675310'
+                     body="From Twilio.  It's a beautiful SMS Message.",
+                     from_=config.TWILIO_PHONE_NUMBER,
+                     to=config.SENT_TO_PHONE_NUMBER
                  )
 
 print(message.sid)
